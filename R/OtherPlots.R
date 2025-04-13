439cab9b37be43e0f77842b9659c9eb35b92b48e
Andrew Lynch <37605960+andrewrlynch@users.noreply.github.com>
2025-04-13 13:55:01 -0500

    Added AnnoFeatures

--- R/OtherPlots.R
+++ R/OtherPlots.R
@@ -1,1 +1,8 @@
-# Wrapper functions for other plotting packages
+# Wrapper functions and extensions for other plotting packages
+
+# OncoPlot-Like
+# Circos-Like
+# Heatmap-Like
+
+# TODO - [ ] Streamline creation of circos plots for rearrangements, copy number, and mutations
+# TODO - [ ] Streamline creation of heatmaps for copy number
